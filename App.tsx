f8dea9afcd0b78f93ac1cc7e3d01e63025b0cab4
Al Riefqy <ekydesmito@gmail.com>
2024-02-07 14:39:41 +0800

    update homepage

--- App.tsx
+++ App.tsx
@@ -76,21 +76,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-            <Text>Hello World</Text>
-          <Section title="Step One">
-          <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text>Hello World</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
